e358774be3daf1f1992806e71a651226be183b15
Sam Phippen <samphippen@googlemail.com>
2012-09-05 19:51:15 +0100

    Replace all socket with @socket
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- lib/http/request_stream.rb
+++ lib/http/request_stream.rb
@@ -9,20 +9,19 @@ module Http
 
     # Stream the request to a socket
     def stream
-      socket = @socket
       @headers.each do |field, value|
         @request_header << "#{field}: #{value}#{CRLF}"
       end
 
       case @body
       when NilClass
-        socket << @request_header << CRLF
+        @socket << @request_header << CRLF
       when String
         @request_header << "Content-Length: #{@body.length}#{CRLF}" unless @headers['Content-Length']
         @request_header << CRLF
 
-        socket << @request_header
-        socket << @body
+        @socket << @request_header
+        @socket << @body
       when Enumerable
         if encoding = @headers['Transfer-Encoding']
           raise ArgumentError, "invalid transfer encoding" unless encoding == "chunked"
@@ -31,13 +30,13 @@ module Http
           @request_header << "Transfer-Encoding: chunked#{CRLF * 2}"
         end
 
-        socket << @request_header
+        @socket << @request_header
         @body.each do |chunk|
-          socket << chunk.bytesize.to_s(16) << CRLF
-          socket << chunk
+          @socket << chunk.bytesize.to_s(16) << CRLF
+          @socket << chunk
         end
 
-        socket << "0" << CRLF * 2
+        @socket << "0" << CRLF * 2
       else raise TypeError, "invalid @body type: #{@body.class}"
       end
     end
