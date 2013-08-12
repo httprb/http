2ae800bc9c1f4d9e438fd2037b1b460d1c074c80
Erik Michaels-Ober <sferik@gmail.com>
2013-08-12 14:04:48 +0200

    Move METHODS constant inside the HTTP::Request class

--- lib/http/request.rb
+++ lib/http/request.rb
@@ -1,5 +1,23 @@
 module HTTP
   class Request
+    # RFC 2616: Hypertext Transfer Protocol -- HTTP/1.1
+    METHODS = [:options, :get, :head, :post, :put, :delete, :trace, :connect]
+
+    # RFC 2518: HTTP Extensions for Distributed Authoring -- WEBDAV
+    METHODS.concat [:propfind, :proppatch, :mkcol, :copy, :move, :lock, :unlock]
+
+    # RFC 3648: WebDAV Ordered Collections Protocol
+    METHODS.concat [:orderpatch]
+
+    # RFC 3744: WebDAV Access Control Protocol
+    METHODS.concat [:acl]
+
+    # draft-dusseault-http-patch: PATCH Method for HTTP
+    METHODS.concat [:patch]
+
+    # draft-reschke-webdav-search: WebDAV Search
+    METHODS.concat [:search]
+
     # Method is given as a lowercase symbol e.g. :get, :post
     attr_reader :method
 
