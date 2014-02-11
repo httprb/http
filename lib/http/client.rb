2b1ebcf7e21ca15e3107b1d973956b07ff954417
Mike Evans <mike@urlgonomics.com>
2014-02-11 14:26:01 -0500

    Allow using URI instances for request building.

--- lib/http/client.rb
+++ lib/http/client.rb
@@ -21,7 +21,7 @@ module HTTP
     # Make an HTTP request
     def request(verb, uri, options = {})
       opts = @default_options.merge(options)
-      host = URI.parse(uri).host
+      host = URI(uri).host
       opts.headers['Host'] = host
       headers = opts.headers
       proxy = opts.proxy
