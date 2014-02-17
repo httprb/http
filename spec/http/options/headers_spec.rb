3b3334024e93593f412633b0464ecd6d9f16f518
Aleksey V Zapparov <ixti@member.fsf.org>
2014-02-17 21:34:18 +0100

    Use headers mixin in options

--- spec/http/options/headers_spec.rb
+++ spec/http/options/headers_spec.rb
@@ -12,7 +12,7 @@ describe HTTP::Options, 'headers' do
   it 'may be specified with with_headers' do
     opts2 = opts.with_headers('accept' => 'json')
     expect(opts.headers).to eq('User-Agent' => user_agent)
-    expect(opts2.headers).to eq('accept' => 'json', 'User-Agent' => user_agent)
+    expect(opts2.headers).to eq('Accept' => 'json', 'User-Agent' => user_agent)
   end
 
   it 'accepts any object that respond to :to_hash' do
