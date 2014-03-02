d8a954f85b373f1438ca4b3db2d377476ba6f7f9
Joe Winter <jwinter@gmail.com>
2014-03-02 17:26:00 -0500

    Add a URI() method that accepts URI objects to backports

--- lib/http/backports/uri.rb
+++ lib/http/backports/uri.rb
@@ -129,3 +129,11 @@ module URI
     ary
   end
 end
+
+module Kernel
+  def URI(uri) # rubocop:disable MethodName
+    return uri if uri.is_a?(URI::Generic)
+    URI.parse(uri)
+  end
+  module_function :URI
+end
