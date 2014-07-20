aad8b6415e784fda747e7efffd6c3bc3f3815592
Erik Michaels-Ober <sferik@gmail.com>
2014-07-20 20:41:38 +0200

    Don't require SimpleCov on Ruby 1.8

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,13 @@
-require 'simplecov'
-require 'coveralls'
+if RUBY_VERSION >= '1.9'
+  require 'simplecov'
+  require 'coveralls'
 
-SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
+  SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
 
-SimpleCov.start do
-  add_filter '/spec/'
-  minimum_coverage(80)
+  SimpleCov.start do
+    add_filter '/spec/'
+    minimum_coverage(80)
+  end
 end
 
 require 'http'
