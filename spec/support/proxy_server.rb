9aabb025b7b3b392e9e34476a5a1f0fcc99fc495
Oleg Kovalenko <okovalenko@cogniance.com>
2015-01-20 16:28:06 +0200

    Start and stop proxies inside the tests that depend on them.

--- spec/support/proxy_server.rb
+++ spec/support/proxy_server.rb
@@ -1,38 +1,38 @@
+require "support/black_hole"
 require "webrick/httpproxy"
 
-handler = proc { |_, res| res["X-PROXIED"] = true }
+def with_proxy(port, handler, options = {})
+  proxy = WEBrick::HTTPProxyServer.new({
+    :Port => port,
+    :AccessLog => [],
+    :ProxyContentHandler => handler,
+    :Logger => BlackHole
+  }.merge(options))
 
-ProxyServer = WEBrick::HTTPProxyServer.new(
-  :Port => 8080,
-  :AccessLog => [],
-  :RequestCallback => handler
-)
+  oversee_webrick_server(proxy) { yield proxy }
+end
 
-AuthenticatedProxyServer = WEBrick::HTTPProxyServer.new(
-  :Port => 8081,
-  :ProxyAuthProc => proc do | req, res |
-    WEBrick::HTTPAuth.proxy_basic_auth(req, res, "proxy") do | user, pass |
-      user == "username" && pass == "password"
+def with_auth_proxy(port, handler, options = {})
+  username = options.fetch(:user)
+  password = options.fetch(:password)
+
+  auth_proc = proc do | req, res |
+    WEBrick::HTTPAuth.proxy_basic_auth(req, res, "proxy") do |user, pass|
+      user == username && pass == password
     end
-  end,
-  :RequestCallback => handler
-)
-
-RSpec.configure do |config|
-  servers = [
-    ProxyServer,
-    AuthenticatedProxyServer
-  ]
-  threads = []
-
-  config.before :suite do
-    threads.push(*servers.map { |server| Thread.new { server.start } })
-    # wait until servers fully boot up
-    Thread.pass while threads.any? { |t| t.status && t.status != "sleep" }
   end
 
-  config.after :suite do
-    servers.each { |server| server.shutdown }
-    threads.each(&:join).clear
+  with_proxy(port, handler, options.merge(:ProxyAuthProc => auth_proc)) { |proxy| yield proxy }
+end
+
+def oversee_webrick_server(server)
+  thread = Thread.new { server.start }
+  Thread.pass while thread.status != "sleep"
+
+  begin
+    yield server
+  ensure
+    server.shutdown
+    thread.join
   end
 end
