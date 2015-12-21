d1780644d098363a30a6b07e7ed8225197b0cbf2
Tony Arcieri <bascule@gmail.com>
2015-12-20 16:32:45 -0800

    Use new SimpleCov::Formatter::MultiFormatter API

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,10 @@
 require "simplecov"
 require "coveralls"
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
-]
+])
 
 SimpleCov.start do
   add_filter "/spec/"
