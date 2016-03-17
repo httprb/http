f7e028ad8bc6432fc8b1be2670a581f648ec68e1
Alexey Zapparov <ixti@member.fsf.org>
2016-03-17 18:55:21 +0100

    Update known http status codes

--- lib/http/response/status/reasons.rb
+++ lib/http/response/status/reasons.rb
@@ -61,6 +61,7 @@ module HTTP
         428 => "Precondition Required",
         429 => "Too Many Requests",
         431 => "Request Header Fields Too Large",
+        451 => "Unavailable For Legal Reasons",
         500 => "Internal Server Error",
         501 => "Not Implemented",
         502 => "Bad Gateway",
