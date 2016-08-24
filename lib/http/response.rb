73f08164d37ed3db8d08f191f58129380b669c2f
Tiago Cardoso <tiago@restorm.com>
2016-08-24 10:04:12 +0300

    do not needlessly call the method, use the ivar

--- lib/http/response.rb
+++ lib/http/response.rb
@@ -102,7 +102,7 @@ module HTTP
       # http://greenbytes.de/tech/webdav/rfc7230.html#rfc.section.3.3.3
       # Clause 3: "If a message is received with both a Transfer-Encoding
       # and a Content-Length header field, the Transfer-Encoding overrides the Content-Length.        
-      return nil if headers[Headers::TRANSFER_ENCODING]
+      return nil if @headers[Headers::TRANSFER_ENCODING]
 
       value = @headers[Headers::CONTENT_LENGTH]
       return unless value
