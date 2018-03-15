httprb/http

commit 800c581574457b1b20f3125cf45879c507b966c1

Fix #readpartial not respecting max length argument

lib/http/connection.rb
@@ -86,9 +86,11 @@ module HTTP
     def readpartial(size = BUFFER_SIZE)
       return unless @pending_response
 
+      chunk = @parser.get_chunk(size)
+      return chunk if chunk
+
       finished = (read_more(size) == :eof) || @parser.finished?
       chunk    = @parser.chunk
-
       finish_response if finished
 
       chunk.to_s

lib/http/response/parser.rb
@@ -3,7 +3,7 @@
 module HTTP
   class Response
     class Parser
-      attr_reader :headers
+      attr_reader :headers, :chunk
 
       def initialize
         @parser = HTTP::Parser.new(self)
@@ -43,9 +43,17 @@ module HTTP
         end
       end
 
-      def chunk
-        chunk  = @chunk
-        @chunk = nil
+      def get_chunk(size)
+        return if @chunk.nil?
+
+        if @chunk.bytesize <= size
+          chunk  = @chunk
+          @chunk = nil
+        else
+          chunk = @chunk.byteslice(0, size)
+          @chunk[0, size] = ""
+        end
+
         chunk
       end
 

commit 071e157e4cc69cf54698221bf38be88e7fe93ccf

Fix broken HTTP::Connection#readpartial
800c5815 broke `readpartial` method: it returns same chunk twice :(

Ref #454

lib/http/connection.rb
@@ -90,7 +90,7 @@ module HTTP
       return chunk if chunk
 
       finished = (read_more(size) == :eof) || @parser.finished?
-      chunk    = @parser.chunk
+      chunk    = @parser.read(size)
       finish_response if finished
 
       chunk.to_s

lib/http/response/parser.rb
@@ -3,7 +3,7 @@
 module HTTP
   class Response
     class Parser
-      attr_reader :headers, :chunk
+      attr_reader :headers
 
       def initialize
         @parser = HTTP::Parser.new(self)

spec/lib/http/connection_spec.rb
@@ -1,0 +1,63 @@
+# frozen_string_literal: true
+
+RSpec.describe HTTP::Connection do
+  let(:req) do
+    HTTP::Request.new(
+      :verb     => :get,
+      :uri      => "http://example.com/",
+      :headers  => {}
+    )
+  end
+  let(:socket) { double(:connect => nil) }
+  let(:timeout_class) { double(:new => socket) }
+  let(:opts) { HTTP::Options.new(:timeout_class => timeout_class) }
+  let(:connection) { HTTP::Connection.new(req, opts) }
+
+  describe "#read_headers!" do
+    before do
+      connection.instance_variable_set(:@pending_response, true)
+      expect(socket).to receive(:readpartial) do
+        <<-RESPONSE.gsub(/^\s*\| */, "").gsub(/\n/, "\r\n")
+        | HTTP/1.1 200 OK
+        | Content-Type: text
+        |
+        RESPONSE
+      end
+    end
+
+    it "reads data in parts" do
+      connection.read_headers!
+      expect(connection.headers).to eq("Content-Type" => "text")
+    end
+  end
+
+  describe "#readpartial" do
+    before do
+      connection.instance_variable_set(:@pending_response, true)
+      expect(socket).to receive(:readpartial) do
+        <<-RESPONSE.gsub(/^\s*\| */, "").gsub(/\n/, "\r\n")
+        | HTTP/1.1 200 OK
+        | Content-Type: text
+        |
+        RESPONSE
+      end
+      expect(socket).to receive(:readpartial) { "1" }
+      expect(socket).to receive(:readpartial) { "23" }
+      expect(socket).to receive(:readpartial) { "456" }
+      expect(socket).to receive(:readpartial) { "78" }
+      expect(socket).to receive(:readpartial) { "9" }
+      expect(socket).to receive(:readpartial) { "0" }
+      expect(socket).to receive(:readpartial) { :eof }
+      expect(socket).to receive(:closed?) { true }
+    end
+
+    it "reads data in parts" do
+      connection.read_headers!
+      buffer = String.new
+      while (s = connection.readpartial(3))
+        buffer << s
+      end
+      expect(buffer).to eq "1234567890"
+    end
+  end
+end