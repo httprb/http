httprb/http

commit e22c9edecf9842445568d46a47b23a98571d72ad

Bump form-data requirement for ruby-2.7 support
Form data needs to be upgraded to add ruby 2.7 support.

http.gemspec
@@ -30,7 +30,7 @@ Gem::Specification.new do |gem|
   gem.add_runtime_dependency "addressable",    "~> 2.3"
   gem.add_runtime_dependency "http-cookie",    "~> 1.0"
   gem.add_runtime_dependency "http-form_data", "~> 2.0"
-  gem.add_runtime_dependency "http-parser",    "~> 1.2.0"
+  gem.add_runtime_dependency "http-parser",    "~> 2.2.0"
 
   gem.add_development_dependency "bundler", "~> 2.0"
 

commit 511945d252bfb00544030cb0256327afba48e105

Fix typo introduced in e22c9edecf
Accidentally bumped version of http-parser instead of form-data.

http.gemspec
@@ -29,8 +29,8 @@ Gem::Specification.new do |gem|
 
   gem.add_runtime_dependency "addressable",    "~> 2.3"
   gem.add_runtime_dependency "http-cookie",    "~> 1.0"
-  gem.add_runtime_dependency "http-form_data", "~> 2.0"
-  gem.add_runtime_dependency "http-parser",    "~> 2.2.0"
+  gem.add_runtime_dependency "http-form_data", "~> 2.2"
+  gem.add_runtime_dependency "http-parser",    "~> 1.2.0"
 
   gem.add_development_dependency "bundler", "~> 2.0"
 