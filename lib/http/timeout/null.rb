httprb/http

commit ce6b3011bacfa2899dac8933349cd20c0cfc009d

Respect the SSL context's verify_hostname value (#632)

lib/http/timeout/null.rb
@@ -37,7 +37,7 @@ module HTTP
 
         return unless ssl_context.verify_mode == OpenSSL::SSL::VERIFY_PEER
 
-        @socket.post_connection_check(host)
+        @socket.post_connection_check(host) if ssl_context.verify_hostname
       end
 
       # Read from the socket

commit a634d74e78949d88aabc3d911d4b1f28e549ce58

Fix jRuby regression introduced in #632
jRuby is using outdated openssl gem bundled in, which don't have
validate_hostname getter on SSLContext.

lib/http/timeout/null.rb
@@ -36,8 +36,9 @@ module HTTP
         connect_ssl
 
         return unless ssl_context.verify_mode == OpenSSL::SSL::VERIFY_PEER
+        return unless ssl_context.respond_to?(:verify_hostname) && ssl_context.verify_hostname
 
-        @socket.post_connection_check(host) if ssl_context.verify_hostname
+        @socket.post_connection_check(host)
       end
 
       # Read from the socket