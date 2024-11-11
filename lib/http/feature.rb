31baa31eb0b6716a4272c91596baef61bcf42df2
Christian Schmidt <github@chsc.dk>
2024-11-11 23:57:35 +0100

    backport: feat: add raise_error feature (#792)
    Ref: db6863ab48fdd77178078b3128a9db3d0ad6069a

--- lib/http/feature.rb
+++ lib/http/feature.rb
@@ -20,6 +20,7 @@ end
 
 require "http/features/auto_inflate"
 require "http/features/auto_deflate"
-require "http/features/logging"
 require "http/features/instrumentation"
+require "http/features/logging"
 require "http/features/normalize_uri"
+require "http/features/raise_error"
