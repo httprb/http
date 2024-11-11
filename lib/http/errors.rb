db6863ab48fdd77178078b3128a9db3d0ad6069a
Christian Schmidt <github@chsc.dk>
2024-11-11 23:57:35 +0100

    feat: add raise_error feature (#792)

--- lib/http/errors.rb
+++ lib/http/errors.rb
@@ -21,6 +21,17 @@ module HTTP
   # Requested to do something when we're in the wrong state
   class StateError < ResponseError; end
 
+  # When status code indicates an error
+  class StatusError < ResponseError
+    attr_reader :response
+
+    def initialize(response)
+      @response = response
+
+      super("Unexpected status code #{response.code}")
+    end
+  end
+
   # Generic Timeout error
   class TimeoutError < Error; end
 
